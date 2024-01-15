e3520bd6f49ad2f05e713608c12aeb357b3f4c54
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2024-01-15 22:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let input = document.getElementById("input");
+
+function addToInput(value) {
+    input.value += value;
+}
+
+function calculate() {
+    try {
+        input.value = eval(input.value);
+        if (input.value === Infinity) {
+            input.value = "Infinity";
+        } else if (isNaN(input.value)) {
+            input.value = "NaN";
+        }
+    } catch (error) {
+        input.value = "Error";
+    }
+}
+
+function clearInput() {
+    input.value = "";
+}
